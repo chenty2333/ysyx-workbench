98113c71882d4c3b79fd2bdf0061ce4fdfd03955
diannaaav <diannaaav@gmail.com>
2024-09-29 09:59:20 +0800

    NJU-ProjectN/nemu ics2024 initialized
    NJU-ProjectN/nemu 27e934a8c52c4e9ffd0de07aac6cffafa190e49e cpu,decode: remove extra `*` for label address

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,20 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vtop.h>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+static TOP_NAME top;
 
-int main() {
-  printf("Hello, ysyx!\n");
+void nvboard_bind_all_pins(TOP_NAME *top);
+
+int main()
+{
+  nvboard_bind_all_pins(&top);
+  nvboard_init();
+
+  while (1)
+  {
+    nvboard_update();
+    top.eval();
+  }
   return 0;
 }
